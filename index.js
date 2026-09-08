e35cdde99ea44461c171caf9285c375b8c4d8f99
maksu <mmaksi6767@gmail.com>
2026-09-09 02:58:55 +0300

    fix deprecation warning

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ client.on('messageCreate', async (message) => {
   }
 });
 
-client.once('ready', () => {
+client.once('clientReady', () => {   
   console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
